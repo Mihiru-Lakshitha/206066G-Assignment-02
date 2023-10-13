10d6b10e58c1e0ab0a3c44224eb63797df03c81a
Mihiru-Lakshitha <mihiru141@gmail.com>
2023-10-13 13:27:41 +0530

    Added remainder calculation feature

--- calculator.py
+++ calculator.py
@@ -21,6 +21,11 @@ def calculate_percentage(x, percentage):
 def square_root(x):
     return math.sqrt(x)
 
+def calculate_remainder(x, y):
+    if y == 0:
+        return "Cannot calculate remainder when the divisor is zero"
+    return x % y
+
 print("Welcome to the Calculator")
 
 
